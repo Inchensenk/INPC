920968cb082bf5ac17bf003af4e6733895e7035f
Inchensenk <aab8117@gmail.com>
2022-10-13 16:16:05 +0300

    Добавьте файлы проекта.

---
+++ INPC/ObservableObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace INPC
+{
+ 
+ /* 1) Не нужно реализовывать INotifyPropertyChanged если нужно обновлять пользовательский интерфейс из пользовательского интерфейса а источником привязки является обычный старый обьект clr
+    
+  2) Если планируется обновлять значения из кода позади, то реализация INotifyPropertyChanged нужна*/
+ 
+ 
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// атрибут [CallerMemberName] позволяет получить метод или имя свойства вызывающего метода
+        /// </summary>
+        /// <param name="propertyName">Имя свойства</param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+    
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ INPC/PersonModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace INPC
+{
+    public class PersonModel : ObservableObject
+    {
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set 
+			{ 
+				_name = value;
+                OnPropertyChanged();// так как мы используем атрибут [CallerMemberName], то не нужно явно задавать свойство Name в параметре
+            }
+		}
+
+		public PersonModel()
+		{
+			//Создание пулла потоков
+			Task.Run(() => 
+			{
+				while (true)
+				{
+					//Random rnd = new Random();
+					//Name = rnd.Next(1,1000).ToString();
+
+					Debug.WriteLine($"Name: {Name}");
+					//худший способ заставить поток спать, но для тестирования пойдет
+					Thread.Sleep(500);
+				}
+			});
+		}
+
+	}
+}
+
+
